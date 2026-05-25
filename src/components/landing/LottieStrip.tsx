3dd0534eb6d91666e09de93dcf291eab5e50f16e
bsm1010 <ahmed.bessam102@gmail.com>
2026-05-25 21:15:02 +0200

    Add LottieStrip component for animation display

---
+++ src/components/landing/LottieStrip.tsx
@@ -0,0 +1,14 @@
+import Lottie from "lottie-react";
+import animationData from "/orbit.json";
+
+export function LottieStrip() {
+  return (
+    <div className="w-full overflow-hidden">
+      <Lottie
+        animationData={animationData}
+        loop={true}
+        className="w-full"
+      />
+    </div>
+  );
+}
